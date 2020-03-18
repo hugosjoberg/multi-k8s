de65e48c9a6c09cd76452aebbbca89fa3227ac7d
Hugo Sjöberg <hugo.sjoberg88@gmail.com>
2020-03-18 15:24:19 -0700

    Update travis config and add deploy and helm

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t hsjoberg/multi-client:latest -t hsjoberg/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t hsjoberg/mutli-server:latest -t hsjoberg/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t hsjoberg/multi-worker:latest -t hsjoberg/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push hsjoberg/multi-client:latest
+docker push hsjoberg/multi-client:$SHA
+docker push hsjoberg/mutli-server:latest
+docker push hsjoberg/mutli-server:$SHA
+docker push hsjoberg/multi-worker:latest
+docker push hsjoberg/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=hsjoberg/multi-server:$SHA
+kubectl set image deployments/client-deployment server=hsjoberg/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=hsjoberg/multi-worker:$SHA
